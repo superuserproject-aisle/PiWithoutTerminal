b56719f1c7b41b8b973731065f67f0d4e8f1148a
Fortnon <76496058+superuserproject-aisle@users.noreply.github.com>
2020-12-31 13:24:49 -0800

    Add files via upload

---
+++ Update operating system and programs.sh
@@ -0,0 +1,5 @@
+echo Updating... Please wait
+sudo apt update && sudo apt full-upgrade -y
+echo Operating system and/or programs updated
+echo Closing in 10 seconds. Click the X to close now
+sleep 10
