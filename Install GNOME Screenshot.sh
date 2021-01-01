3b628aef97a0290a9c9c1b670911565110e4d7a3
Fortnon <76496058+superuserproject-aisle@users.noreply.github.com>
2020-12-31 16:50:13 -0800

    Update Install GNOME Screenshot.sh

--- Install GNOME Screenshot.sh
+++ Install GNOME Screenshot.sh
@@ -1,4 +1,4 @@
 echo Starting process...
-sudo apt update && sudo apt install gnome-screenshot
+sudo apt update && sudo apt install gnome-screenshot -y
 echo Complete. Closing in 10 seconds
 sleep 10
